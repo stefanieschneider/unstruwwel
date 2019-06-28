210bc398bc4e9a0f96d67a89ad909cac18ca5860
Stefanie Schneider <stefanie.schneider@cereality.net>
2019-06-28 16:41:26 +0200

    Add test data

---
+++ data-test/schemes.R
@@ -0,0 +1,27 @@
+require(stringr)
+require(dplyr)
+
+get_scheme <- function(file) {
+  data <- tibble::enframe(readRDS(file))
+
+  scheme <- filter(data, !is.na(value)) %>%
+    mutate(
+      scheme = str_replace_all(value, "[0-9]", "9") %>%
+        str_replace_all("[\\p{L}]+", "..."),
+      language = str_match(file, "([^/]+).rds$") %>%
+        purrr::pluck(2, .default = "")
+    ) %>%
+    group_by(scheme) %>% sample_n(1) %>%
+    filter(!str_detect(value, "\\\\")) %>%
+    select(value, scheme, language)
+
+  return(ungroup(scheme))
+}
+
+valid_files <- "^[a-z]{2}.rds$" # excludes midas
+
+schemes <- list.files("./data-test", valid_files,
+    full.names = TRUE, ignore.case = TRUE) %>%
+  purrr::map(get_scheme) %>% bind_rows()
+
+usethis::use_data(schemes, overwrite = TRUE)
